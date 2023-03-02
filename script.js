13434f8cf0fa0b88650202cbd82522b5cab9c6fb
manny <emmanuelgomezrealpro@gmail.com>
2023-03-02 18:17:14 -0500

    added functions

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = (prompt("password length 8-128?"));
+  if (passwordLength > 128 || passwordLength < 8) {
+    generatePassword()
+  }
+
+
+
+
+  var specialCharacters = confirm("do you want to use special characters?");
+
+  var upperCharacters = confirm("do you want upper case?");
+  var lowerCharacters = confirm("do you want lower case?");
+  var numCharacters = confirm("do you want numbers?");
+
+  
+
+
+console.log(upperCharacters)
+if (specialCharacters === false && upperCharacters === false && lowerCharacters === false && numCharacters === false){
+  generatePassword()
+}
+  return "password"
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- 
-  passwordText.value = password;
-  var index = Math.floor(Math.random() * PushSubscriptionOptions.length);
-  var computerChoice = Option [index];
 
+  passwordText.value = password;
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function generatePassword()
-{
-  var passwordLength = prompt ( "password length?")
-  var specialCharacters =confirm("do want to use special characters?")
-  var numbers =confirm("do you want to use numbers?")
-  var doYouwanttocontinue =confirm("do you want to continue creating custom password")
-  
-
-}
